59a04c5c1d7f792c3ec57f9061735e2a7f5926e7
Xylecrack <dhruvakodiadka@gmail.com>
2025-06-16 23:11:53 +0530

    verified commit?

---
+++ leetcode.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    class Solution {
+        public:
+            int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+                for (int i = 0; i < dominoes.size(); i++)
+                {
+                    
+                }
+
+            }
+        };
+    return 0;
+}
+// 1,3,1,2,2
+// 1: 2
+// 3:1
+// 2:2
+// total = 1+1c2
+// 4:4
+// total: 1+2+3+4
+// 1,3,3,4,5
+// 1,3,4,5
+//
