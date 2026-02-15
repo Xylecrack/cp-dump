64a82f6a83ec2700cc55e94c4a54cb77a3490947
Dhruva <dhruvakodiadka@gmail.com>
2026-02-15 19:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.76%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.cpp
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int solve(int n, vector<int>& dp) {
+        if (n <= 2)
+            return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return dp[n];
+    }
+
     int climbStairs(int n) {
-        if (n <= 2){ return n;}
-        int a = 1;
-        int b = 2;
-        for (int i = 3; i <= n; i++) {
-            int temp = a + b;
-            a = b;
-            b = temp;
-        }
-        return b;
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
