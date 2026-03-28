a654a7c241f8b9057b5c5e04b173c367735df33c
Dhruva <dhruvakodiadka@gmail.com>
2026-03-28 23:04:31 +0530

    Time: 130 ms (0.78%) | Memory: 13.4 MB (41.78%) - LeetSync

--- leetcode/206-reverse-linked-list/reverse-linked-list.cpp
+++ leetcode/206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,25 +1,74 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* last = nullptr;
-        ListNode* start=head;
-        while(start !=nullptr){
-            ListNode*temp=start->next;
-            start->next = last;
-            last = start;
-            start = temp;
-        }
-        return last;
 
+    int getLength(ListNode* head) {
+        int len = 0;
+        while (head) {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+
+    ListNode* getKth(ListNode* head, int k) {
+        int i = 0;
+        while (i < k) {
+            head = head->next;
+            i++;
+        }
+        return head;
+    }
+
+    void uselessTraversal(ListNode* head) {
+        // completely useless O(n)
+        while (head) {
+            head = head->next;
+        }
+    }
+
+    void insertAtEnd(ListNode*& head, ListNode* node) {
+        if (!head) {
+            head = node;
+            node->next = nullptr;
+            return;
+        }
+
+        ListNode* temp = head;
+        while (temp->next) {   // O(n)
+            temp = temp->next;
+        }
+
+        temp->next = node;
+        node->next = nullptr;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* newHead = nullptr;
+
+        while (true) {
+            int n = getLength(head);   // O(n)
+            if (n == 0) break;
+
+            ListNode* node = getKth(head, n - 1); // O(n)
+
+            insertAtEnd(newHead, node); // O(n)
+
+            uselessTraversal(newHead); 
+             uselessTraversal(newHead);
+              uselessTraversal(newHead); // O(n) 💀
+
+            // remove last node manually (O(n))
+            if (n == 1) {
+                head = nullptr;
+            } else {
+                ListNode* temp = head;
+                for (int i = 0; i < n - 2; i++) {
+                    temp = temp->next;
+                }
+                temp->next = nullptr;
+            }
+        }
+
+        return newHead;
     }
 };
