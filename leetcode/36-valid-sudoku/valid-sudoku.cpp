8bbd14839f413e2d9985efba9701d67d51bd5a26
Dhruva <dhruvakodiadka@gmail.com>
2026-03-31 20:26:04 +0530

    Time: 5 ms (33.51%) | Memory: 25.8 MB (12.39%) - LeetSync

--- leetcode/36-valid-sudoku/valid-sudoku.cpp
+++ leetcode/36-valid-sudoku/valid-sudoku.cpp
@@ -3,37 +3,22 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_map<int, int> sudorow[9];
-        for (int row = 0; row < 9; row++) {
-            for (auto& it : board[row]) {
-                if (it == '.')
-                    continue;
-                if (sudorow[row][it - '0'] == 1)
-                    return false;
-                sudorow[row][it - '0']++;
-            }
-        }
         unordered_map<int, int> sudocol[9];
-        for (int col = 0; col < 9; col++) {
-            for (auto& it : board) {
-                if (it[col] == '.')
-                    continue;
-                if (sudocol[col][it[col] - '0'] == 1)
-                    return false;
-                sudocol[col][it[col] - '0']++;
-            }
-        }
         unordered_map<int, int> sudoblock[9];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
+                int n = (i / 3) * 3 + (j / 3);
                 if (board[i][j] == '.')
                     continue;
-                int n = (i / 3) * 3 + (j / 3);
-                if (sudoblock[n][board[i][j] - '0'] == 1)
+                int num = board[i][j] - '0';
+                if (sudorow[i][num] == 1 || sudocol[j][num] == 1 ||
+                    sudoblock[n][num] == 1)
                     return false;
-                sudoblock[n][board[i][j] - '0']++;
+                sudorow[i][num]++;
+                sudocol[j][num]++;
+                sudoblock[n][num]++;
             }
         }
-
         return true;
     }
 };
